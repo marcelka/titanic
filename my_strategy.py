a4685ea54d90e03277b651636cc20ab8394d0ce5
Marcela Hrda <marcelka.hrda@gmail.com>
2015-03-28 19:07:21 +0100

    Renaming.

--- my_strategy.py
+++ my_strategy.py
@@ -4,7 +4,7 @@ import numpy as np
 class Passenger:
     """
     PassengerId: 891
-    Survived: 0
+    Survived: 0 -> bool
     Pclass: 3
     Name: Dooley, Mr. Patrick
     Sex: male
@@ -38,28 +38,24 @@ class Passenger:
     def row(self):
         return [getattr(self, attr) for attr in self.header]
 
-
 def to_array(passengers):
     return np.array([p.row() for p in passengers])
-
 
 def test_strategy(strategy, passengers):
     correct = [p for p in passengers if strategy(p) == p.Survived]
     return len(correct) / len(passengers)
 
-
-def get_csv(strategy, passengers):
-    with open('results/my_strategy.csv', 'w', newline='') as csvfile:
+def save_csv(strategy, filename, passengers):
+    with open('results/%s' % filename, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',',)
         writer.writerow(['PassengerId', 'Survived'])
         for p in passengers:
             writer.writerow([p.PassengerId, int(strategy(p))])
 
-
 def cheaty(passenger):
     return passenger.Survived
 
-def model(passenger):
+def gender(passenger):
     return passenger.Sex == 'female'
 
 passengers = []
@@ -74,20 +70,19 @@ fares = {c: np.median([p.Fare for p in passengers if p.Pclass == c and
          p.Fare is not None]) for c in classes}
 age = np.median([p.Age for p in passengers if p.Age is not None])
 for p in passengers:
-    if p.Fare is None:
-        p.Fare = fares[p.Pclass]
-    if p.Age is None:
-        p = age
+    if p.Fare is None: p.Fare = fares[p.Pclass]
+    if p.Age is None: p = age
 
 print('cheaty', test_strategy(cheaty, passengers))
-print('model', test_strategy(model, passengers))
-get_csv(model, passengers)
+print('gender', test_strategy(gender, passengers))
+
+save_csv(gender, "my-gender-train.csv", passengers)
 
 with open('data/test.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     header = next(reader)
     passengers = [Passenger(header, row) for row in reader]
-    get_csv(model, passengers)
+    save_csv(gender, "my-gender-test.csv", passengers)
     print(to_array(passengers))
 
 
